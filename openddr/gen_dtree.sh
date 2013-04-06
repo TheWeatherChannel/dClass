390245c6feab13db4551878dcceb79b4cb907dcb
Reza Naghibi <reza.naghibi@yahoo.com>
2013-04-06 16:19:53 -0400

    readmes

--- openddr/gen_dtree.sh
+++ openddr/gen_dtree.sh
@@ -3,10 +3,16 @@
 OPENDDR_RESOURCE_PATH=/some/path/OpenDDR/latest/resources
 DCLASS_BIN=../src/dclass_client
 
+if [ ! -d "$OPENDDR_RESOURCE_PATH" ]
+then
+    echo "ERROR: invalid OpenDDR resource path: $OPENDDR_RESOURCE_PATH"
+    exit 1
+fi
+
 if [ ! -s "$DCLASS_BIN" ]
 then
-        echo "ERROR: dclass_client binary not found"
-        exit 1
+    echo "ERROR: dclass_client binary not found"
+    exit 1
 fi
 
 cp BuilderDataSourcePatch.xml DeviceDataSourcePatch.xml "$OPENDDR_RESOURCE_PATH"
